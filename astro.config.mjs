12bf3613b783526295ef387fab5200f7eda4f846
segfaulty1 <segfaulty@proton.me>
2022-12-17 21:08:15 +0100

    feat: added projects component to portfolio section, using astro/image package. resolve #5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import react from "@astrojs/react";
+import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), image({serviceEntryPoint: '@astrojs/image/sharp'})]
 });
