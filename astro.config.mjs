e764d45c5538a507f73312882fb02053b5b89c0a
segfaulty1 <segfaulty@proton.me>
2022-12-25 04:47:45 +0100

    deployment: trying to figure out why netlify doesn not show images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import image from '@astrojs/image';
+// import image from '@astrojs/image';
 // import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  })],
+  integrations: [tailwind(), react()],
   // output: "server",
   // adapter: netlify()
 });
