580f695f62bd8b72ca26b1f752251f9eae8feaff
segfaulty1 <segfaulty@proton.me>
2022-12-25 03:53:28 +0100

    minor: adapting configs to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import image from "@astrojs/image";
+import {defineConfig} from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [tailwind(), react(), image({serviceEntryPoint: '@astrojs/image/sharp'})]
+    integrations: [
+        tailwind(),
+        react(),
+        image({serviceEntryPoint: '@astrojs/image/sharp'}),
+    ],
 });
