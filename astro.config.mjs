d170345f29311f6f21271796aaef6e74da06bcdf
segfaulty1 <segfaulty@proton.me>
2022-12-25 03:58:26 +0100

    feat: setting up multi lang feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import {defineConfig} from 'astro/config';
+import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import image from '@astrojs/image';
+// import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        react(),
-        image({serviceEntryPoint: '@astrojs/image/sharp'}),
-    ],
+  integrations: [tailwind(), react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })],
+  // output: "server",
+  // adapter: netlify()
 });
