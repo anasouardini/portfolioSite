defad081a962cffad4ada50f8a892c1a0323c8ed
segfaulty1 <segfaulty@proton.me>
2023-06-11 00:05:56 +0100

    enhancement: added files compression; removed some console.logs; ditched the internationalizing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,12 @@ import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anasouardini.online',
-  integrations: [
-    tailwind(),
-    react(),
-    image({serviceEntryPoint: '@astrojs/image/sharp'}),
-    sitemap(),
-    robotsTxt()
-  ]
+  integrations: [tailwind(), react(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap(), robotsTxt(), compress()]
 });
