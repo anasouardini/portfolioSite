932e44f13f3ae568cf8e12b769fc65d706489bd4
segfaulty1 <segfaulty@proton.me>
2023-09-03 15:03:48 +0100

    fix: fixed a stale astro.config file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anasouardini.online',
-  integrations: [tailwind(), react(), sitemap(), robotsTxt() ],
+  integrations: [tailwind(), sitemap(), robotsTxt() ],
   server: {port: 3000}
 });
