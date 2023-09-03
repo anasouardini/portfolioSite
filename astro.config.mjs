19c4e101d95ff5ae38ae309a9f70c6751e9c3664
segfaulty1 <segfaulty@proton.me>
2023-09-03 14:50:58 +0100

    upgrade: upgraded to new Astro release, image optimization is built-in now.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-
-import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anasouardini.online',
-  integrations: [tailwind(), react(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), robotsTxt(), compress()]
+  integrations: [tailwind(), react(), sitemap(), robotsTxt() ],
+  server: {port: 3000}
 });
