ce70da4bde447519ccf5887f9907958f166541e3
anasouardini <ouardinianas@proton.me>
2024-01-09 23:06:00 +0100

    minor

--- public/installer/dropper.sh
+++ public/installer/dropper.sh
@@ -2,20 +2,26 @@
 
 # make sure curl is there
 if ! command -v curl &>/dev/null; then
+  echo "installing curl";
   sudo apt install curl -y
 fi
 
 #-------- install deno
 # deno needs unzip to install itself
 if ! command -v unzip &>/dev/null; then
+  echo "installing unzip";
   sudo apt install unzip -y
 fi
 
+echo "running the deno installer";
 curl -fsSL https://deno.land/x/install/install.sh | bash
 export PATH="$PATH:$HOME/.deno/bin/"
 
 #------------ decrypt and run the installer TS script
-curl -fsSL https://anasouardini.online/installer/installer.enc;
+echo "download encrypted post-installation script";
+curl -fsSL https://anasouardini.online/installer/installer.enc -o installer.enc;
 # TODO: decryption modifies the file
-openssl enc -aes-256-cbc -d -in ./installer.enc -out ./installer.sh;
-deno run --allow-all ./installer.sh;
+echo "decrypting...";
+openssl enc -aes-256-cbc -d -in ./installer.enc -out ./installer.ts;
+echo "running the post-installation script";
+deno run --allow-all ./installer.ts;
